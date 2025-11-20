a0433256a4ad30b05f2712d16c3732e99fdb8cc5
Mateo Solano <115440962+MateoSolano65@users.noreply.github.com>
2025-11-20 16:50:20 -0500

    Feat: Add CodigoDane property to PA_PrioSedeBeneficiarias model and create SQL script for alterations

--- SISPAE.Model/Model/PA_PrioSedeBeneficiarias.cs
+++ SISPAE.Model/Model/PA_PrioSedeBeneficiarias.cs
@@ -83,6 +83,12 @@ namespace SISPAE.Model
         public String? Sede{ get; set; }
 
         /// <Resumen>  
+        /// ( Gets or Sets ) Obtiene o establece el valor de la propiedad: CodigoDane 
+        /// </Resumen> 
+        [Display(Name = "Codigo Dane")]   
+        public String? CodigoDane{ get; set; }
+
+        /// <Resumen>  
         /// ( Gets or Sets ) Obtiene o establece el valor de la propiedad: PriorizadaPAE 
         /// </Resumen> 
         [Display(Name = "Priorizada P A E")] 
